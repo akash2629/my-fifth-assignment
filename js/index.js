9b28d3a155dbc38eeae0b4d6990a1f736f569099
akash2629 <atiqulislamakash1@gmail.com>
2024-02-19 12:08:35 +0600

    added jscript file

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let totalPrice = parseInt(document.getElementById('totalPrice').innerText);
+
+let ticketCount = parseInt(document.getElementById('ticketCount').innerText);
+
+
+let totalSeat = parseInt(document.getElementById('totalSeat').innerText);
+
